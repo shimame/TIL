2b664939d61723749aef1b58990f64bf2f602231
shimame <92255702+shimame@users.noreply.github.com>
2023-12-20 13:27:42 +0900

    Delete IntroductionToAlgorithmsPyhton/ChapterQuestion2_1.py

--- IntroductionToAlgorithmsPyhton/ChapterQuestion2_1.py
+++
@@ -1,6 +0,0 @@
-for i in range(1950, 2051):
-    if(i % 4 == 0):
-        if(i % 100 == 0):
-            if(i % 400 == 0):
-                print(str(i) + "年\n")
-        else: print(str(i) + "年\n")
