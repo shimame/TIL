b58f65965a8758bd9cce0e843aeea4a5b66c34a0
shimame <adcshun143@gmail.com>
2023-12-20 17:27:29 +0900

    SelectionSort

---
+++ IntroductionToAlgorithmsPyhton/select_sort.py
@@ -0,0 +1,11 @@
+data = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
+
+for i in range(len(data)):
+    min = i
+    for j in range(i + 1, len(data)):
+        if data[min] > data[j]:
+            min = j
+    
+    #最小値minの位置と現在iの要素を交換
+    data[i], data[min] = data[min], data[i]
+print(data)
