08255bc0c780798a8388cf7aee4710270822d6fe
shimame <adcshun143@gmail.com>
2024-02-03 19:08:22 +0900

    SoftmaxFunctionofAdaptationOverflow

---
+++ 0startDeepLearning/Softmax_adaptation_overflow.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+a = np.array([1010, 1000, 990])
+print(softmax(a))
+
+"""overflow_error
+exp_a = np.exp(a)
+print(exp_a)
+sum_exp_a = np.sum(exp_a)
+print(sum_exp_a)
+y = exp_a / sum_exp_a
+print(y)
+"""
