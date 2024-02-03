355d1c5a9b9b09235aebe53c40483cc7699ae4d5
shimame <adcshun143@gmail.com>
2024-02-03 18:22:43 +0900

    NeuralNetworkof3Layers

--- 0startDeepLearning/neural_network0-3.py
+++ 0startDeepLearning/neural_network0-3.py
@@ -7,24 +7,24 @@ def identity_function(x):
 
 def init_network():
     network = {}
-    network['w1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
     network['b1'] = np.array([0.1, 0.2, 0.3])
-    network['w2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
     network['b2'] = np.array([0.1, 0.2])
-    network['w3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
     network['b3'] = np.array([0.1, 0.2])
 
     return network
 
 def forward(network, x):
-    w1, w2, w3 = network['w1'], network['w2'], network['w3']
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-    a1 = np.dot(x, w1) + b1
+    a1 = np.dot(x, W1) + b1
     z1 = sigmoid(a1)
-    a2 = np.dot(z1, w2) + b2
+    a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
-    a3 = np.dot(z2, w3) + b3
+    a3 = np.dot(z2, W3) + b3
     y = identity_function(a3)
 
     return y
