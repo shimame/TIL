a7b3d62606df50d52b14d58249a2d8bf6574dcfe
shimame <adcshun143@gmail.com>
2024-02-09 23:54:31 +0900

    DealingWithBatchDatum

--- 0startDeepLearning/cross_entropy_error.py
+++ 0startDeepLearning/cross_entropy_error.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
 y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
