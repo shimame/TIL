5a93c0eb907d3ebb3687e0fdb6565610d240a7b3
shimame <adcshun143@gmail.com>
2024-04-15 20:09:23 +0900

    NumDifferential

---
+++ 0startDeepLearning/numerical_differentiation.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def numerical_diff(f, x):
+    h = 1e-4 # 0.0001
+    return (f(x+h) - f(x-h)) / (2*h)
+
+    """
+    h = 1e-50   丸め誤差が発生する
+    return (f(x+h) - f(x)) / h  微分誤差が発生する
+    """
