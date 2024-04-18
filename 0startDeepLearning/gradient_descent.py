a5ba2d54dd5bc5637bb8d39d98d91d0f2bca247b
shimame <adcshun143@gmail.com>
2024-04-18 15:35:08 +0900

    calculation gradient descent

---
+++ 0startDeepLearning/gradient_descent.py
@@ -0,0 +1,15 @@
+from numerical_gradienet import numerical_gradient
+from function_2 import function_2
+import numpy as np
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
