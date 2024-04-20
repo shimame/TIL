07faed8cee0ca7c8f3005007d3932329a7068036
shimame <adcshun143@gmail.com>
2024-04-20 23:17:02 +0900

    gradient simplenet

---
+++ 0startDeepLearning/gradient_simplenet.py
@@ -0,0 +1,29 @@
+import numpy as np
+from softmax_function import softmax
+from cross_entropy_error import cross_entropy_error
+from numerical_gradienet import numerical_gradient
+
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+x = np.array([0.6, 0.9])
+t = np.array([0, 0, 1])
+
+net = simpleNet()
+
+f = lambda w: net.loss(x, t)
+dW = numerical_gradient(f, net.W)
+
+print(dW)
